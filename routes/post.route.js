be21c52b0282bb84834958fd7ebb7adae85bc496
Gurnoor638 <gurnoorarora28@yahoo.com>
2026-07-16 18:03:40 +0530

    update post route

--- routes/post.route.js
+++ routes/post.route.js
@@ -16,7 +16,7 @@ const router = express.Router();
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
-router.post("/like/:id", protectRoute, likeUnlikePostePost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
 router.get("/all", protectRoute, getAllPosts);
 router.get("/likes/:id", protectRoute, getLikedPosts);
 router.get("/following", protectRoute, getFollowingPosts);
