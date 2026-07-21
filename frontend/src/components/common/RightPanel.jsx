9b490869beb377eb81651343481b1ae78bb223c2
Gurnoor638 <gurnoorarora28@yahoo.com>
2026-07-21 17:11:05 +0530

    useFollow hook added -3

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
-import RightPanelSkeleton from "../skeletons/RightPanelSkeleton.jsx";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
+
+import useFollow from "../../hooks/useFollow.jsx";
+
+import RightPanelSkeleton from "../skeletons/RightPanelSkeleton.jsx";
+import LoadingSpinner from "./LoadingSpinner.jsx";
 
 const RightPanel = () => {
 	const queryClient = useQueryClient();
@@ -23,6 +27,8 @@ const RightPanel = () => {
 			}
 		}
 	});
+
+	const {follow, isPending} = useFollow();
 
 	if (suggestedUsers?.length === 0) return <div className='md:w-64 w-0'></div>;
 
@@ -50,7 +56,7 @@ const RightPanel = () => {
 								<div className='flex gap-2 items-center'>
 									<div className='avatar'>
 										<div className='w-8 rounded-full'>
-											<img src={user.profileImg || "/avatar-placeholder.png"} />
+											<img src={user.profile_img || "/avatar-placeholder.png"} />
 										</div>
 									</div>
 									<div className='flex flex-col'>
@@ -63,9 +69,12 @@ const RightPanel = () => {
 								<div>
 									<button
 										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => e.preventDefault()}
+										onClick={(e) => {
+											e.preventDefault();
+											follow(user.id);
+										}}
 									>
-										Follow
+										{isPending ? <LoadingSpinner size="sm" /> : "Follow"}
 									</button>
 								</div>
 							</Link>
